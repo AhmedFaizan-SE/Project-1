2aeb819e85734ddaec457dab05d1249abd5a11ad
Ahmed Faizan <ahmedfaizan@gmail.com>
2026-05-11 19:08:39 +0500

    Added listing file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
  let port = 8080;
 
  const mongo_URL = 'mongodb://127.0.0.1:27017/myapp';
-
+ const listing = require("./models/listing.js");
  main()
  .then(() => {
     console.log("DB connected");
@@ -22,6 +22,19 @@ app.get("/", (req, res) => {
     res.send("Hi");
 })
 
+app.get("/testlisting", async(req,res) => {
+   let sample = new listing ({
+    title: "Vella",
+    description: "xyz",
+    price: 1200,
+    image: "",
+    location: "California",
+    country: "USA"
+   });
+   await sample.save();
+   console.log("saved");
+   res.send("Successful");
+});
  app.listen(port, ()=>{
 
     console.log ("Listening");
