a993f4bc23e270d501add651d02ae5f86c8f74c4
Ahmed Faizan <ahmedfaizan@gmail.com>
2026-05-15 03:16:55 +0500

    Add listing views and show route

--- app.js
+++ app.js
@@ -6,7 +6,10 @@
  let port = 8080;
 
  const mongo_URL = 'mongodb://127.0.0.1:27017/myapp';
- const listing = require("./models/listing.js");
+ app.use(express.urlencoded({extended:true}));
+ 
+ const path = require("path");
+
  main()
  .then(() => {
     console.log("DB connected");
@@ -22,19 +25,35 @@ app.get("/", (req, res) => {
     res.send("Hi");
 })
 
-app.get("/testlisting", async(req,res) => {
-   let sample = new listing ({
-    title: "Vella",
-    description: "xyz",
-    price: 1200,
-    image: "",
-    location: "California",
-    country: "USA"
-   });
-   await sample.save();
-   console.log("saved");
-   res.send("Successful");
+const Listing = require("./models/listing.js");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings}); 
 });
+
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+});
+
+
+// app.get("/testlisting", async(req,res) => {
+//    let sample = new listing ({
+//     title: "Vella",
+//     description: "xyz",
+//     price: 1200,
+//     image: "",
+//     location: "California",
+//     country: "USA"
+//    });
+//    await sample.save();
+//    console.log("saved");
+//    res.send("Successful");
+// });
  app.listen(port, ()=>{
 
     console.log ("Listening");
