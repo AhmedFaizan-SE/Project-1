54aa79055d76eca7f8df1e08ee3917bf300719a0
Ahmed Faizan <ahmedfaizan@gmail.com>
2026-05-15 18:44:39 +0500

    Add listing create and edit routes

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
- //requiring express
+ // App setup
  const express = require("express");
  const app = express();
- //requiring moongoose
  const mongoose = require("mongoose");
+ const methodOverride = require("method-override");
  let port = 8080;
 
+ // Connect to local MongoDB database
  const mongo_URL = 'mongodb://127.0.0.1:27017/myapp';
  app.use(express.urlencoded({extended:true}));
+ app.use(methodOverride("_method"));
  
  const path = require("path");
 
@@ -29,11 +31,38 @@ const Listing = require("./models/listing.js");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// Show all listings
 app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings}); 
 });
 
+// new route
+
+app.get("/listings/new" , (req, res) => {
+    res.render("listings/new.ejs"); 
+})
+
+// creating new route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+app.get("/listings/:id/edit", async (req, res)=> {
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs" , {listing})
+});
+
+app.put("/listings/:id", async (req, res) => {
+ let {id} = req.params;
+await Listing.findByIdAndUpdate(id, {...req.body.listing});
+res.redirect("/listings");
+});
+
+// Show details for one listing
 app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
